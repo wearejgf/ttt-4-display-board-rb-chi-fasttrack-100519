913abf79046edda168037f65d9ce8ddfc376c480
xiujun <19485823@qq.com>
2019-10-06 03:08:07 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,33 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+'''
+def display_board(board)
+  puts "-" * 12
+  puts board[0]+"|"+board[1]+"|"+board[2]+"|"
+  puts "-" * 12
+  puts board[3]+"|"+board[4]+"|"+board[5]+"|"
+  puts "-" * 12
+  puts board[6]+"|"+board[7]+"|"+board[8]+"|"
+  puts "-" * 12
+  
+end
+'''
+
+
+def display_board(board)
+  #puts "-" * 12
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-" * 11
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-" * 11
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  #puts "-" * 12
+  
+end
+
+#board=["111"," "," "," "," ","555"," "," ","999"]
+#board=["111","222","333","444","555","666","777","888","999"]
+
+
+#display_board board
