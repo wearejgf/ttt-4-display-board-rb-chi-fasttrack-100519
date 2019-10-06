506c6d575b4f0771b0895d919d9de61c9ecdcf28
xiujun <19485823@qq.com>
2019-10-06 03:36:36 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -19,9 +19,15 @@ def display_board(board)
   #puts "-" * 12
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-" * 11
+<<<<<<< HEAD
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-" * 11
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
+=======
+  puts " #{board[3]} |#{board[4]} | #{board[5]} "
+  puts "-" * 11
+  puts " #{board[6]} |#{board[7]} | #{board[8]} "
+>>>>>>> ec8c380ca38b3c237a8fa5816477c32d0b32c48e
   #puts "-" * 12
   
 end
